71faad514399d120b5da1a07d20adeb93c2503df
Kalina Frontend <dzhiltcov@yandex.ru>
2023-02-05 22:14:11 +0200

    feat: реализовать api user

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,33 @@
 import React from "react";
 import "../index.css";
+import api from "../utils/Api";
 
 function Main({isEditProfilePopupOpen, isAddPlacePopupOpen, isEditAvatarPopupOpen}) {
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription ] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+  api.getUserInfo().then( data=> {
+    setUserName(data.name);
+    setUserDescription(data.about);
+    setUserAvatar(data.avatar);
+  })
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__card">
-          <button className="profile__avatar-edit-button" type="button" onClick={isEditAvatarPopupOpen}>
             <img
-              src="<%=require('./images/profile-avatar.jpg')%>"
+              src={userAvatar}
               alt="Аватар пользователя"
               className="profile__avatar"
             />
-          </button>
+          <button className="profile__avatar-edit-button" type="button" onClick={isEditAvatarPopupOpen}></button>
           <div className="profile__info">
             <div className="profile__info-block">
-              <h1 className="profile__info-name">Жак-Ив Кусто</h1>
+              <h1 className="profile__info-name">{userName}</h1>
               <button type="button" className="profile__edit-button" onClick={isEditProfilePopupOpen}></button>
             </div>
-            <p className="profile__job">Исследователь океана</p>
+            <p className="profile__job">{userDescription}</p>
           </div>
         </div>
         <button type="button" className="profile__add-button" onClick={isAddPlacePopupOpen}></button>
